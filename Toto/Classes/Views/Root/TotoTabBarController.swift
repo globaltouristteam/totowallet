1f0675b5544fd3e177d4a53d5f374da9aa41860d
Nhuan Vu <nhuanvd@live.com>
2018-09-15 00:00:00 +0700

    Revert "Config tab"
    This reverts commit 987ebc76a83cf5a7d64c16795a38a0ea3d66be77.

--- Toto/Classes/Views/Root/TotoTabBarController.swift
+++ Toto/Classes/Views/Root/TotoTabBarController.swift
@@ -21,17 +21,7 @@ class TotoTabBarController: UITabBarController {
             addEmptyTabs()
         }
     }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        (tabBar as? TotoTabBar)?.updateTotoTab()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        (tabBar as? TotoTabBar)?.updateTotoTab()
-    }
-    
+
     func addWalletTabs() {
         var controllers: [UIViewController] = []
         if let tours = viewControllers?.first {
